f974673693acee975d69180d3346bf14ba11ae75
agl3118 <adam.lau18@imperial.ac.uk>
2021-03-02 22:53:04 +0800

    Changed nuclear png

--- config/config.js
+++ config/config.js
@@ -33,7 +33,7 @@ Config.customcolors = {
 	'zarel': 'aeo'
 };
 /*** Begin automatically generated configuration ***/
-Config.version = "0.11.2 (ef302fa5)";
+Config.version = "0.11.2 (040c0cc4)";
 
 Config.routes = {
 	root: 'crunchy-leagueserver.herokuapp.com',
