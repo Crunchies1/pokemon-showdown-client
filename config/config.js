efd986cd880c7697d2488f99f90b780abc894a7b
agl3118 <adam.lau18@imperial.ac.uk>
2021-03-05 02:21:24 +0800

    added spirtes

--- config/config.js
+++ config/config.js
@@ -33,7 +33,7 @@ Config.customcolors = {
 	'zarel': 'aeo'
 };
 /*** Begin automatically generated configuration ***/
-Config.version = "0.11.2 (90248af3)";
+Config.version = "0.11.2 (dc779783)";
 
 Config.routes = {
 	root: 'crunchy-leagueserver.herokuapp.com',
