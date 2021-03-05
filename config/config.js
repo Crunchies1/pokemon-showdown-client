7524fc2bf9312e18edfd608d092f3f0bb1c8e248
agl3118 <adam.lau18@imperial.ac.uk>
2021-03-05 21:23:00 +0800

    Fixed sprite dimens

--- config/config.js
+++ config/config.js
@@ -33,7 +33,7 @@ Config.customcolors = {
 	'zarel': 'aeo'
 };
 /*** Begin automatically generated configuration ***/
-Config.version = "0.11.2 (efd986cd)";
+Config.version = "0.11.2 (05582250)";
 
 Config.routes = {
 	root: 'crunchy-leagueserver.herokuapp.com',
