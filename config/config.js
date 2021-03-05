38006b42611733bcfbd32fb45abadaac2c9454be
agl3118 <adam.lau18@imperial.ac.uk>
2021-03-05 21:24:50 +0800

    Fixed background

--- config/config.js
+++ config/config.js
@@ -33,7 +33,7 @@ Config.customcolors = {
 	'zarel': 'aeo'
 };
 /*** Begin automatically generated configuration ***/
-Config.version = "0.11.2 (05582250)";
+Config.version = "0.11.2 (7524fc2b)";
 
 Config.routes = {
 	root: 'crunchy-leagueserver.herokuapp.com',
